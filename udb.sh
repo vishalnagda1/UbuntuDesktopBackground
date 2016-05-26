1c8a90a9f49be75dbbc8a7343a5c13a94be23068
Vishal Nagda <vishalnagda1@users.noreply.github.com>
2016-05-26 11:11:15 +0530

    UDB.sh

--- udb.sh
+++ udb.sh
@@ -2,12 +2,11 @@
 # Author : Vishal Nagda
 # Help For This File :
 # OPEN TERMINAL AND TYPE sudo -i AND NOW ENTER YOUR ACCOUNT PASSWORD.
-# NOW LOCATE THE DIRECOTRY THAT CONTAIN THIS FILE.
+# NOW LOCATE THE DIRECOTRY THAT CONTAIN UDB.sh FILE.
 # COPY THIS FILE AND ALL YOUR NEW BACKGROUND IMAGES TO THE DIRECTORY /usr/share/backgrounds/
-# NOW OPEN THE DIRECTORY /usr/share/backgrounds/contest (THIS DIRECTORY MAY BE DIFFERENT IN YOUR CASE,
-# COZ I'M USING UBUNTU 16.04 LTS i.e. XENIAL XERUS).
-# MAKE COPY OF THE ORIGINAL FILE xenial.xml (THIS FILE MAY ALSO BE DIFFERENT IN YOUR CASE).
-# NOW GO BACK TO THE PREVIOUS DIRECTORY AND RUN THIS FILE BY TYPING IN TERMINAL ./udb.sh
+# NOW LOCATE THE DIRECTORY /usr/share/backgrounds/contest (THIS DIRECTORY MAY BE DIFFERENT IN YOUR CASE, COZ I'M USING UBUNTU 16.04 LTS i.e. XENIAL XERUS).
+# MAKE A COPY OF THE ORIGINAL FILE xenial.xml (THIS FILE MAY ALSO BE DIFFERENT IN YOUR CASE).
+# NOW GO BACK TO THE PREVIOUS DIRECTORY AND RUN UDB.sh FILE BY TYPING IN TERMINAL ./UDB.sh
 # DONE
 
 SAVEIFS=$IFS
@@ -19,12 +18,12 @@ TRANSITION=5.0
 
 # Random order
 IMGS=($(ls *.{jpg,jpeg,png,gif,JPG,JPEG,PNG,GIF} 2>/dev/null | sort -R))
-COUNTER=`expr ${#IMGS[*]} - 1`
+T_IMG=`expr ${#IMGS[*]} - 1`
 CURRENTDIR=$(pwd)
 
 echo -e "<background>\n  <starttime>\n    <year>2009</year>\n    <month>08</month>\n    <day>04</day>\n    <hour>00</hour>\n    <minute>00</minute>\n    <second>00</second>\n  </starttime>\n<!-- This animation will start at midnight. -->" > $FILE
 
-for ((i=0;  i<$COUNTER; i++))
+for ((i=0;  i<$T_IMG; i++))
 do
     echo -e "  <static>\n    <duration>$DURATION</duration>\n    <file>$CURRENTDIR/${IMGS[$i]}</file>\n  </static>" >> $FILE
     echo -e "  <transition>\n    <duration>$TRANSITION</duration>\n    <from>$CURRENTDIR/${IMGS[$i]}</from>" >> $FILE
